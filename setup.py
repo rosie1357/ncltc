26c5db088526f743d619ddb6eaabdd45871c0c45
Rosalie Malsberger <RMalsberger@mathematica-mpr.com>
2021-08-30 13:33:23 -0400

    build: add initial setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nctcl_project_code',
+    version='0.1.0',
+    description='Package to create NC databasetables /measures/anaylsis',
+    packages=find_packages("nc_code"), 
+    package_dir={"": "nc_code"},
+    author='Rosalie Malsberger', 
+    author_email='rmalsberger@mathematica-mpr.com',
+    license='Mathematica',
+    entry_points={
+        'console_scripts': [
+            # add when ready
+        ],
+    },
+)
