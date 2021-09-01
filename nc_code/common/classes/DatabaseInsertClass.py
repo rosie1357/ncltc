51e79acba49914a8f91c5603d942370fc71e18a7
Rosalie Malsberger <RMalsberger@mathematica-mpr.com>
2021-09-01 11:27:39 -0400

    feat: initial working code for DatabaseInsert to create insert statements

--- nc_code/common/classes/DatabaseInsertClass.py
+++ nc_code/common/classes/DatabaseInsertClass.py
@@ -13,26 +13,47 @@ class DatabaseInsert(Database):
 
     """
     DatabaseInsert class - child of Database class to use to input dataframe and database table layout 
-    and create insert statements to be passed to batch execute to insert multiple records into database table
+    and create insert statements and identify any mismatches, can then pass insert statements to batch execute
     
     """
 
-    def __init__(self, df, layout, db_params):
-        self.df, self.layout = df, layout
+    def __init__(self, df, layout_df, db_params):
+        
+        self.df, self.layout_df = df, layout_df
         super().__init__(db_params)
 
-    def create_inserts(row, colmapping):
+        self.colmapping_dict = self.gen_layout_mapping()
+
+        self.sql_parameter_sets, self.mismatches = self.gen_batch_inserts()
+
+    def gen_layout_mapping(self):
+        """
+        method gen_layout_mapping to use df and layout_df passed attributes to
+        create a dictionary mapping columns from df to db formats
+
+        assumes the following:
+            - self.layout_df has columns 'colname' and 'sqltype' that map input df/db columns to the database type 
+                (e.g. DATE, CHAR(2), SMALLINT)
+
+            - any columns that should map to the database layout on self.df have the same names 
+                (e.g. if colname = 'CALYEAR' on self.layout_df, the column will also be named 'CALYEAR' on self.df)
+
+        the method will identify all columns that are both in self.df.columns and listed in a record under 'colname' in self.layout_df and return
+        a dictionary mapping colname to sqltype to pass to create_insert_statement() method
+        
+        
+        """
+
+        return {col : sqltype for col, sqltype in zip(self.layout_df['colname'], self.layout_df['sqltype']) if col in self.df.columns}
+
+    def create_insert_statement(self, row):
     
         """
-        create_inserts method to be applied to each row of the input df to then create
+        method create_insert_statement to be applied to each row of the input df to then create
         SQL insert statements mapping df col to db col with row value
 
         params:
             row series: row of df to be inserted, auto-generated when applied to df with axis=1 call
-            colmappings dict: dictionary with mapping of df col to db col with format types
-                the colmappings dict is structured as follows:
-                    key = column name (assumes df column and db column have same name)
-                    value = database SQL type
 
         returns:
             insert list: list of all inserts for given row
@@ -54,7 +75,7 @@ class DatabaseInsert(Database):
         insert = []
         mismatches = []
         
-        for col, sqltype in colmapping.items():
+        for col, sqltype in self.colmapping_dict.items():
             
             # collapse input sqltypes to valid database type
             
@@ -105,7 +126,7 @@ class DatabaseInsert(Database):
 
                 except:
                     mismatches.append({'Input column': col,
-                                    'Input row': row['rownum'],
+                                    'Input row': row.name+1,
                                     'Value': row[col], 
                                     'Database column': col,
                                     'Database format': sqltype})
@@ -115,3 +136,25 @@ class DatabaseInsert(Database):
             insert.append({'name':col, 'value': {dbtype: value}})
 
         return insert, mismatches
+
+    def gen_batch_inserts(self):
+
+        """
+        gen_batch_inserts method to apply create_insert_statement row-wise to instance df to create list of 
+        parameter sets to pass to batch execute, and list of mismatches to print to log
+        
+        """
+
+        insertlists = self.df.apply(self.create_insert_statement, axis=1)
+
+        mismatches = pd.DataFrame()
+        sql_parameter_sets = []
+
+        for i in range(len(insertlists)):
+            sql_parameter_sets.append(insertlists[i][0])
+            mismatch = insertlists[i][1]
+            if len(mismatch) > 0:
+                mismatches = pd.concat([mismatches, pd.DataFrame(mismatch)])
+
+        return sql_parameter_sets, mismatches
+
